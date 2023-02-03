b14ad21da14309eb25a8dfb03c4106565bfcba6d
Emorinken <emorinkensam@gmail.com>
2023-02-03 07:04:33 -0800

    worked on apostrophe error

--- pages/homepage.jsx
+++ pages/homepage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
 				<h3>SO YOU WANT TO TRAVEL TO</h3>
 				<h1>SPACE</h1>
 				<div>
-					<p>Let's face it, if you want to go to space, you might as well go to ou;ter space and not hover6 kind of on the edge of it. Well sit back and relax cos we'll give you a truly out of this world experience!</p>
+					<p>Let&apos;s face it, if you want to go to space, you might as well go to outer space and not hover kind of on the edge of it. Well sit back and relax cos we'll give you a truly out of this world experience!</p>
 				</div>
 			</section>
 			<section className='right'>
